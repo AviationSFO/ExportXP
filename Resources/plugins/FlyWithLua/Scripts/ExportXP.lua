7502c362b85afde4d6590696d7bbe7fc0db2c8ab
AviationSFO <srw08sf@gmail.com>
2022-04-29 19:59:54 -0700

    add payload data and acf name and icao

--- Resources/plugins/FlyWithLua/Scripts/ExportXP.lua
+++ Resources/plugins/FlyWithLua/Scripts/ExportXP.lua
@@ -14,6 +14,9 @@ add_macro("Toggle ExportXP", "active = true", "active = false", "deactivate")
 -- from X-Plane's API for later export
 function getData(data)
     -- get the data from the flight model
+    dataref("acf_type", "sim/aircraft/view/acf_ICAO")
+    dataref("acf_name", "sim/aircraft/view/acf_tailnum")
+
     dataref("lat", "sim/flightmodel/position/latitude", "readonly")
     dataref("lon", "sim/flightmodel/position/longitude", "readonly")
     dataref("alt", "sim/flightmodel/position/elevation", "readonly")
@@ -22,9 +25,13 @@ function getData(data)
     dataref("vs", "sim/flightmodel/position/vh_ind_fpm", "readonly")
     dataref("pitch", "sim/flightmodel/position/true_theta", "readonly")
     dataref("roll", "sim/flightmodel/position/phi", "readonly")
+
     dataref("eng1_rpm", "sim/flightmodel/engine/ENGN_N1_[0]", "readonly")
     dataref("eng2_rpm", "sim/flightmodel/engine/ENGN_N1_[1]", "readonly")
+
     dataref("fuel_wgt", "sim/flightmodel/weight/m_fuel_total", "readonly")
+    dataref("payload_wgt", "sim/flightmodel/weight/m_fixed", "readonly")
+
 
     -- Applying to data array
     data.lat = lat
@@ -38,6 +45,10 @@ function getData(data)
     data.eng1_rpm = eng1_rpm
     data.eng2_rpm = eng2_rpm
     data.fuel_wgt = fuel_wgt
+    data.payload_wgt = payload_wgt
+
+    data.acf_type = acf_type
+    data.acf_name = acf_name
 
     return data
 end
@@ -51,6 +62,7 @@ function exportData(data)
     file:close()
     -- adding data
     file = io.open("ExportXP.txt", "a")
+    file:write(data.acf_type .. " " .. data.acf_name .. "\n")
     file:write("-!- POSITION/FLIGHT MODEL DATA -!-\n")
     file:write(data.lat, ",\n")
     file:write(data.lon, ",\n")
@@ -63,8 +75,9 @@ function exportData(data)
     file:write("-!- ENGINE DATA -!-\n")
     file:write(data.eng1_rpm, ",\n")
     file:write(data.eng2_rpm, ",\n")
-    file:write("-!- FUEL DATA -!-\n")
+    file:write("-!- WEIGHT DATA -!-\n")
     file:write(data.fuel_wgt, ",\n")
+    file:write(data.payload_wgt, ",\n")
     file:close()
 end
 
